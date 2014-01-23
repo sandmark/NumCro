2e2c33385d0f8319cd3604f1fcce735156d4b3ab
sandmark <sandmark.m@gmail.com>
2014-01-23 09:27:33 -0800

    パース・代入を実装

--- lib/numcro.rb
+++ lib/numcro.rb
@@ -2,6 +2,8 @@
 require "active_support"
 require "active_support/core_ext"
 
+class InvalidFormatError < Exception; end
+
 class NumberCross
   attr_accessor :x, :y, :answer_length, :answer_numbers
   def initialize
@@ -13,24 +15,71 @@ class NumberCross
     @sheet = nil
   end
 
-  def create_sheet
-    raise RuntimeError, "instanse has zero size." if size_zero?
+  def create_sheet(sym=nil)
+    raise RuntimeError, "instance has zero size." if size_zero?
+    if not @sheet or sym == :force
+      @sheet = Array.new(@y).map{ Array.new(@x, 0) }
+    end
+  end
+
+  def parse(lines)
+    lines.split(/\n/).each.with_index{|line,i| parse_line(i, line)}
+  end
+
+  def place(n, s)
+    if not s.size == 1
+      raise RuntimeError, "#{s} must be one character."
+    elsif not @sheet.flatten.include?(n)
+      raise IndexError, "#{n} is not a member of numbers."
+    end
+    @numbers[n] = s
+  end
+
+  def x=(n)
+    @x = n if integer?(n)
+    create_sheet if not size_zero?
+  end
+
+  def y=(n)
+    @y = n if integer?(n)
+    create_sheet if not size_zero?
+  end
+
+  def integer?(n)
+    if not n.kind_of?(Integer)
+      raise TypeError, "#{n.inspect} is not an Integer"
+    elsif n <= 0
+      raise RuntimeError, "#{n} is not a positive integer."
+    else
+      true
+    end
+  end
+
+  def has_sheet?
+    @sheet ? true : false
   end
 
   def to_s
     create_sheet
     lines = hr
-    @y.times do
-      lines += hr(true)
+    @y.times do |x|
+      lines += hr(x)
     end
     lines += hr
   end
 
-  def hr(normal=false)
-    if normal
-      "|" + Array.new(@x).inject(""){|r,i| r+="  |"} + "\n"
+  def hr(y=nil)
+    if y
+      "|" + @sheet[y].inject(""){ |r, n|
+        if @numbers.has_key?(n)
+          r += "#{sprintf('%02d', @numbers[x])}|}"
+        else
+          cell = n.zero? ? "**" : sprintf("%02d", n)
+          r += "#{cell}|"
+        end
+      } + "\n"
     else
-      "+" + Array.new(@x).inject(""){|r,i| r+="--+"} + "\n"
+      "+" + Array.new(@x).inject(""){|r| r+="--+"} + "\n"
     end
   end
 
@@ -48,13 +97,27 @@ class NumberCross
   }.join("|")
   end
 
-  def parse_line(i, string)
+  def parse_line(y, string)
+    create_sheet
+    raise IndexError, "out of index" if y >= @y
+    indices = split_indices(string)
+    raise IndexError, "out of index" if not indices.size == @x
+    indices.each.with_index do |n, x|
+      @sheet[y][x] = n
+    end
+  end
 
+  def split_indices(string)
+    if not string.match(/^[0-9\.]+$/)
+      raise InvalidFormatError, "format contains not numbers or dots."
+    end
+    string.split(/\./).map(&:to_i)
   end
 
   def size_zero?
     @x.blank? or @y.blank? or @x.zero? or @y.zero?
   end
 
-  private :hr, :parse_line, :create_sheet, :size_zero?
+  private :hr, :parse_line, :create_sheet, :size_zero?, :integer?
+  private :split_indices
 end
--- spec/numcro_spec.rb
+++ spec/numcro_spec.rb
@@ -6,12 +6,64 @@ describe "ナンクロ" do
   before :all do
     X = 5
     Y = 3
+    OUT_OF_INDEX = 3
+    InvalidSeq = "1.2.3.4.a.b.c"
+    ONE_TO_FIVE = <<EOS
++--+--+--+--+--+
+|01|02|03|04|05|
+|01|02|03|04|05|
+|01|02|03|04|05|
++--+--+--+--+--+
+EOS
+    ONE_ALL = <<EOS
++--+--+--+--+--+
+|01|01|01|01|01|
+|01|01|01|01|01|
+|01|01|01|01|01|
++--+--+--+--+--+
+EOS
+    BLANK_SHEET = <<EOS
++--+--+--+--+--+
+|**|**|**|**|**|
+|**|**|**|**|**|
+|**|**|**|**|**|
++--+--+--+--+--+
+EOS
   end
 
   before :each do
     @numcro = NumberCross.new
+    @zero   = NumberCross.new
     @numcro.x = X
     @numcro.y = Y
+  end
+
+  describe "integer?" do
+    it "数値以外の場合例外を投げる" do
+      expect{@zero.send(:integer?, :a)}.to raise_error(TypeError)
+    end
+
+    it "整数の場合は真を返す" do
+      expect(@zero.send(:integer?, 1)).to be_true
+    end
+
+    it "負の整数の場合は例外を投げる" do
+      expect{@zero.send(:integer?, -1)}.to raise_error
+    end
+
+    it "Float型は受け付けない" do
+      expect{@zero.send(:integer?, 1.0)}.to raise_error(TypeError)
+    end
+  end
+
+  describe "サイズ" do
+    it "xは数値以外受け付けない" do
+      expect{@zero.x=:a}.to raise_error(TypeError)
+    end
+
+    it "yは数値以外受け付けない" do
+      expect{@zero.y=:a}.to raise_error(TypeError)
+    end
   end
 
   describe "例外" do
@@ -33,28 +85,9 @@ describe "ナンクロ" do
       it "x,yともにnilの場合真を返す" do
         expect(@enumcro.send(:size_zero?)).to be_true
       end
-
-      it "xが0の場合真を返す" do
-        @enumcro.x = 0
-        @enumcro.y = 1
-        expect(@enumcro.send(:size_zero?)).to be_true
-      end
-
-      it "yが０の場合真を返す" do
-        @enumcro.y = 0
-        @enumcro.x = 1
-        expect(@enumcro.send(:size_zero?)).to be_true
-      end
     end
 
     it "サイズ nil でシートは作れない" do
-      expect{@enumcro.send(:create_sheet)}
-        .to raise_error(RuntimeError)
-    end
-
-    it "サイズ 0 でシートは作れない" do
-      @enumcro.x = 0
-      @enumcro.y = 0
       expect{@enumcro.send(:create_sheet)}
         .to raise_error(RuntimeError)
     end
@@ -63,20 +96,138 @@ describe "ナンクロ" do
       expect{@enumcro.to_s}.to raise_error(RuntimeError)
     end
 
-    it "サイズ 0 を to_s" do
-      @enumcro.x = 0
-      @enumcro.y = 0
-      expect{@enumcro.to_s}.to raise_error(RuntimeError)
+    it "size_zero? が真のとき parse_line は例外を投げる" do
+      expect{@enumcro.send(:parse_line, 0, "1.2.3.4.5")}
+        .to raise_error(RuntimeError)
     end
   end
 
   describe "シート" do
+    before :each do
+      @sheet = @numcro.instance_eval("@sheet")
+      @numbers = @numcro.instance_eval("@numbers")
+    end
 
-    it "一行ずつパース" do
-      @numcro.send(:parse_line, "10.1.2.0.4", 0)
-      expect(@numcro.send(:hr, 0)).to eq <<EOS
+    describe "create_sheet" do
+      it "シートが存在する場合は何もしない" do
+        @numcro.parse("1.2.3.4.5\n"*3)
+        expect(@numcro.instance_eval("@sheet")).to be_kind_of(Array)
+        @numcro.to_s
+        expect(@numcro.to_s).to eq ONE_TO_FIVE
+      end
+
+      it ":forceが指定された場合上書きする" do
+        @numcro.parse("1.1.1.1.1\n"*3)
+        expect(@numcro.to_s).to eq ONE_ALL
+        @numcro.send(:create_sheet, :force)
+        expect(@numcro.to_s).to eq BLANK_SHEET
+      end
+    end
+
+    describe "place" do
+      before :each do
+        @numcro.parse <<EOS
+1.2.3.4.5
+6.7.8.9.10
+11.12.13.14.15
+EOS
+      end
+
+      it "数字に文字を関連付ける" do
+        @numcro.place(1, "あ")
+        expect(@numbers[1]).to eq "あ"
+      end
+
+      it "数値はparse_lineで入力された範囲内でなければならない" do
+        expect{@numcro.place(1, "あ")}.to_not raise_error
+        expect{@numcro.place(16,"あ")}.to raise_error(IndexError)
+      end
+
+      it "文字は一文字でなければならない" do
+        expect{@numcro.place(1,"あい")}.to raise_error
+        expect{@numcro.place(1,"")}.to raise_error
+      end
+    end
+
+    it "x,yが指定された時点でシートを作る" do
+      expect(@numcro.has_sheet?).to be_true
+    end
+
+    it "Integerの二次元配列" do
+      expect(@sheet).to be_kind_of(Array)
+      @sheet.each do |line|
+        expect(line).to be_kind_of(Array)
+        line.each do |cell|
+          expect(cell).to be_kind_of(Integer)
+        end
+      end
+    end
+
+    it "一次配列はY個の要素を持つ" do
+      expect(@sheet.size).to eq Y
+    end
+
+    it "二次配列はX個の要素を持つ" do
+      @sheet.each do |line|
+        expect(line.size).to eq X
+      end
+    end
+
+    describe "parse_line" do
+      it "複数行はparseがまとめて処理する" do
+        @another = NumberCross.new
+        @another.x = X
+        @another.y = Y
+        @numcro.y.times do |i|
+          @numcro.send(:parse_line, i, "1.2.3.4.5")
+        end
+        @another.parse("1.2.3.4.5\n"*3)
+        expect(@another.to_s).to eq @numcro.to_s
+        expect(@another.to_s).to eq ONE_TO_FIVE
+      end
+
+      it "行末に改行文字が含まれている場合は無視する" do
+        expect{@numcro.send(:parse_line, 0, "1.2.3.4.5\n")}.
+          to_not raise_error
+        sheet = @numcro.instance_eval("@sheet")
+        expect(sheet[0][0]).to eq 1
+      end
+
+      it "範囲外のインデックスを渡されたとき例外を投げる" do
+        expect{
+          @numcro.send(:parse_line, OUT_OF_INDEX, "1.2.3.4.5")
+        }.to raise_error(IndexError)
+      end
+
+      it "文字列がインデックスの範囲外だった場合例外を投げる" do
+        expect {
+          @numcro.send(:parse_line, 0, "1.2.3.4.5.10")
+        }.to raise_error(IndexError)
+
+        expect {
+          @numcro.send(:parse_line, 0, "1")
+        }.to raise_error(IndexError)
+      end
+
+      it "数字とドット以外の文字が含まれている場合例外を投げる" do
+        expect{@numcro.send(:split_indices, "1.2.3")}.
+          to_not raise_error
+
+        expect{@numcro.send(:split_indices, InvalidSeq)}.
+          to raise_error(InvalidFormatError)
+      end
+
+      it "負の整数が渡された場合例外を投げる" do
+        expect{@numcro.send(:split_indices, "1.2.-3.4")}.
+          to raise_error(InvalidFormatError)
+      end
+
+      it "一行ずつパース" do
+        @numcro.send(:parse_line, 0, "10.1.2.0.4")
+        expect(@numcro.send(:hr, 0)).to eq <<EOS
 |10|01|02|**|04|
 EOS
+      end
     end
   end
 
@@ -84,9 +235,9 @@ EOS
     it "シートを表示" do
       expect(@numcro.to_s).to eq <<EOS
 +--+--+--+--+--+
-|  |  |  |  |  |
-|  |  |  |  |  |
-|  |  |  |  |  |
+|**|**|**|**|**|
+|**|**|**|**|**|
+|**|**|**|**|**|
 +--+--+--+--+--+
 EOS
     end
